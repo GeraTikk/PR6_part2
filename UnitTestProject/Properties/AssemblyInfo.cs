0f846de49778de1c7ee17f51ddeb6e8608fe9100
GeraTikk <geratikk@yandex.ru>
2025-04-10 02:54:31 +0300

    Итог

---
+++ UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e1ae00a7-f00e-4c6a-94fd-310ff343d3f3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject/UnitTest2.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GermanKursach;
+using System;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void AuthTest()
+        {
+            var authWindow = new MainWindow();
+            Assert.IsTrue(authWindow.Auth("client1", "pass123"));
+
+        }
+    }
+}
